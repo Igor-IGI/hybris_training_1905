3b39e293db13a1b6d06114998f08e017de6e4966
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-03 13:54:41 +0200

    add core service interface for customer

---
+++ bin/custom/trainingsocialsitecore/src/de/hybris/platform/trainingsocialsitecore/service/ITrainingSocialSiteCoreService.java
@@ -0,0 +1,11 @@
+package de.hybris.platform.trainingsocialsitecore.service;
+
+import de.hybris.platform.core.model.user.CustomerModel;
+
+import java.util.List;
+
+public interface ITrainingSocialSiteCoreService {
+
+    public List<CustomerModel> getCutomers();
+    public CustomerModel getCustomerById(String CustomerId);
+}
