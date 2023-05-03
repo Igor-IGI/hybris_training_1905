186f2ebd49dab9326a9e0302a168303b008b9657
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-03 08:34:06 +0200

    run ant clean all

---
+++ bin/custom/trainingsocialsitecore/gensrc/de/hybris/platform/trainingsocialsitecore/constants/GeneratedTrainingsocialsitecoreConstants.java
@@ -0,0 +1,59 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 3 May 2023, 08:23:47                        ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ * Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved.
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package de.hybris.platform.trainingsocialsitecore.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingsocialsitecoreConstants
+{
+	public static final String EXTENSIONNAME = "trainingsocialsitecore";
+	public static class TC
+	{
+		public static final String SOCIALSITE = "SocialSite".intern();
+		public static final String SOCIALSITEENUM = "SocialSiteEnum".intern();
+	}
+	public static class Attributes
+	{
+		public static class Customer
+		{
+			public static final String SOCIALSITES = "socialSites".intern();
+		}
+	}
+	public static class Enumerations
+	{
+		public static class SocialSiteEnum
+		{
+			public static final String FACEBOOK = "Facebook".intern();
+			public static final String INSTAGRAM = "Instagram".intern();
+			public static final String TWITTER = "Twitter".intern();
+			public static final String TIKTOK = "TikTok".intern();
+			public static final String LINKEDIN = "LinkedIn".intern();
+			public static final String PINTEREST = "Pinterest".intern();
+		}
+	}
+	public static class Relations
+	{
+		public static final String CUSTOMERTOSOCIALSITE = "CustomerToSocialSite".intern();
+	}
+	
+	protected GeneratedTrainingsocialsitecoreConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
