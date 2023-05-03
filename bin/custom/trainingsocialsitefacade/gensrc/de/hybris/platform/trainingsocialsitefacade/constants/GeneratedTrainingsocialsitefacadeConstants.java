b0fca15097844eb6e4043994a05c6ba147a1b48d
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-03 11:51:00 +0200

    run ant clean all

--- bin/custom/trainingsocialsitefacade/gensrc/de/hybris/platform/trainingsocialsitefacade/constants/GeneratedTrainingsocialsitefacadeConstants.java
+++ bin/custom/trainingsocialsitefacade/gensrc/de/hybris/platform/trainingsocialsitefacade/constants/GeneratedTrainingsocialsitefacadeConstants.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at 3 May 2023, 10:30:32                        ---
+ * --- Generated at 3 May 2023, 11:43:30                        ---
  * ----------------------------------------------------------------
  *  
  * [y] hybris Platform
