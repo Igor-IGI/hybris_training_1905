ab39d53cd18cce99e693fb4017321f8ee095acd2
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-03 14:34:37 +0200

    add facade object

---
+++ bin/custom/trainingsocialsitefacade/src/de/hybris/platform/trainingsocialsitefacade/facade/impl/TrainingSocialSiteFacade.java
@@ -0,0 +1,45 @@
+package de.hybris.platform.trainingsocialsitefacade.facade.impl;
+
+import de.hybris.platform.commercefacades.user.data.CustomerData;
+import de.hybris.platform.converters.Converters;
+import de.hybris.platform.core.model.user.CustomerModel;
+import de.hybris.platform.servicelayer.dto.converter.Converter;
+import de.hybris.platform.trainingsocialsitecore.service.impl.TrainingSocialSiteCoreService;
+import de.hybris.platform.trainingsocialsitefacade.facade.ITrainingSocialSiteFacade;
+
+import java.util.List;
+
+public class TrainingSocialSiteFacade implements ITrainingSocialSiteFacade {
+
+    private TrainingSocialSiteCoreService courseService;
+    private Converter<CustomerModel, CustomerData> trainingCustomerConverter;
+
+    @Override
+    public List<CustomerData> getCustomers() {
+        //Convert CustomerModel to CustomerData
+        return Converters.convertAll(getCourseService().getCutomers(), getTrainingCustomerConverter());
+    }
+
+    @Override
+    public CustomerData getCustomerById(String customerId) {
+        //Convert CustomerModel to CustomerData
+        return getTrainingCustomerConverter().convert(getCourseService().getCustomerById(customerId));
+    }
+
+    public Converter<CustomerModel, CustomerData> getTrainingCustomerConverter() {
+        return trainingCustomerConverter;
+    }
+
+    public void setTrainingCustomerConverter(Converter<CustomerModel, CustomerData> trainingCustomerConverter) {
+        this.trainingCustomerConverter = trainingCustomerConverter;
+    }
+
+    public TrainingSocialSiteCoreService getCourseService() {
+        return courseService;
+    }
+
+    public void setCourseService(TrainingSocialSiteCoreService courseService) {
+        this.courseService = courseService;
+    }
+
+}
