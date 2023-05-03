d6cf8b7aba14e9b1495aee118dc481303d35f396
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-03 09:39:28 +0200

    run ant all

---
+++ bin/custom/trainingsocialsitefacade/gensrc/de/hybris/platform/trainingsocialsitefacade/constants/GeneratedTrainingsocialsitefacadeConstants.java
@@ -0,0 +1,31 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 3 May 2023, 09:33:50                        ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ * Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved.
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package de.hybris.platform.trainingsocialsitefacade.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingsocialsitefacadeConstants
+{
+	public static final String EXTENSIONNAME = "trainingsocialsitefacade";
+	
+	protected GeneratedTrainingsocialsitefacadeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
