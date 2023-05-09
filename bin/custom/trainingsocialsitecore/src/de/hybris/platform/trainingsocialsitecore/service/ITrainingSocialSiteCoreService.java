a99734b4819b881272fbc5feb9aa02086ddab91c
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-09 09:54:37 +0200

    change collection to list

--- bin/custom/trainingsocialsitecore/src/de/hybris/platform/trainingsocialsitecore/service/ITrainingSocialSiteCoreService.java
+++ bin/custom/trainingsocialsitecore/src/de/hybris/platform/trainingsocialsitecore/service/ITrainingSocialSiteCoreService.java
@@ -2,6 +2,7 @@ package de.hybris.platform.trainingsocialsitecore.service;
 
 import de.hybris.platform.core.model.user.CustomerModel;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface ITrainingSocialSiteCoreService {
