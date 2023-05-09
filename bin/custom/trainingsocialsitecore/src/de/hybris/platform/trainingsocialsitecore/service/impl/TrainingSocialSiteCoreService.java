38966c20dfb06c4dae464d12cae3af695732a6b6
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-09 08:56:28 +0200

    modify service to use interface instead object to use dependency injection

--- bin/custom/trainingsocialsitecore/src/de/hybris/platform/trainingsocialsitecore/service/impl/TrainingSocialSiteCoreService.java
+++ bin/custom/trainingsocialsitecore/src/de/hybris/platform/trainingsocialsitecore/service/impl/TrainingSocialSiteCoreService.java
@@ -1,23 +1,34 @@
 package de.hybris.platform.trainingsocialsitecore.service.impl;
 
 import de.hybris.platform.core.model.user.CustomerModel;
+import de.hybris.platform.trainingsocialsitecore.dao.ITrainingSocialSiteCustomDao;
 import de.hybris.platform.trainingsocialsitecore.dao.impl.TrainingSocialSiteCustomerDaoImpl;
 import de.hybris.platform.trainingsocialsitecore.service.ITrainingSocialSiteCoreService;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 public class TrainingSocialSiteCoreService implements ITrainingSocialSiteCoreService {
 
-    private TrainingSocialSiteCustomerDaoImpl trainingSocialSiteDao;
-
+    @Resource(name = "trainingSocialSiteDao")
+    private ITrainingSocialSiteCustomDao trainingSocialSiteDao;
 
     @Override
     public List<CustomerModel> getCutomers() {
-        return (List<CustomerModel>) trainingSocialSiteDao.getCustomers();
+        final List<CustomerModel> customers = (List<CustomerModel>) getTrainingSocialSiteDao().getCustomers();
+        return customers;
     }
 
     @Override
     public CustomerModel getCustomerById(String customerId) {
         return trainingSocialSiteDao.getCustomerById(customerId);
+    }
+
+    public ITrainingSocialSiteCustomDao getTrainingSocialSiteDao() {
+        return trainingSocialSiteDao;
+    }
+
+    public void setTrainingSocialSiteDao(ITrainingSocialSiteCustomDao trainingSocialSiteDao) {
+        this.trainingSocialSiteDao = trainingSocialSiteDao;
     }
 }
