c907f54748eba2cd70f77cb6aff8a03ecc7cb232
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-11 12:30:05 +0200

    add FaqFacadeImpl

---
+++ bin/custom/trainingsocialsitefacade/src/de/hybris/platform/trainingsocialsitefacade/facade/impl/TrainingFaqFacadeImpl.java
@@ -0,0 +1,14 @@
+package de.hybris.platform.trainingsocialsitefacade.facade.impl;
+
+import de.hybris.platform.trainingsocialsitefacade.data.FaqData;
+import de.hybris.platform.trainingsocialsitefacade.facade.ITrainingFaqFacade;
+
+import java.util.List;
+
+public class TrainingFaqFacadeImpl implements ITrainingFaqFacade
+{
+    @Override
+    public List<FaqData> getFaq() {
+        return null;
+    }
+}
