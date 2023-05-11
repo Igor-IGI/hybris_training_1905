6ef7ac33c40b9656d788b40ef49602ef48ece8e4
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-11 11:14:06 +0200

    add FAQ core service interface

---
+++ bin/custom/trainingsocialsitecore/src/de/hybris/platform/trainingsocialsitecore/service/ITrainingFaqCoreService.java
@@ -0,0 +1,10 @@
+package de.hybris.platform.trainingsocialsitecore.service;
+
+import de.hybris.platform.trainingsocialsitecore.model.FaqModel;
+
+import java.util.List;
+
+public interface ITrainingFaqCoreService {
+
+    public List<FaqModel> getFaqs();
+}
