110e1bcd784faa70dd24a96fe60330c93ba0f340
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-11 09:48:28 +0200

    add putFaq methode

--- bin/custom/trainingsocialsitecore/src/de/hybris/platform/trainingsocialsitecore/dao/impl/TrainingFaqDaoImpl.java
+++ bin/custom/trainingsocialsitecore/src/de/hybris/platform/trainingsocialsitecore/dao/impl/TrainingFaqDaoImpl.java
@@ -1,22 +1,24 @@
 package de.hybris.platform.trainingsocialsitecore.dao.impl;
 
-import de.hybris.platform.core.model.user.CustomerModel;
 import de.hybris.platform.servicelayer.internal.dao.AbstractItemDao;
+import de.hybris.platform.servicelayer.search.FlexibleSearchQuery;
 import de.hybris.platform.servicelayer.search.SearchResult;
 import de.hybris.platform.trainingsocialsitecore.dao.ITrainingFaqDao;
 import de.hybris.platform.trainingsocialsitecore.model.FaqModel;
-import de.hybris.platform.trainingsocialsitecore.service.impl.DefaultTrainingsocialsitecoreService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
+import static de.hybris.platform.servicelayer.util.ServicesUtil.validateParameterNotNull;
 
 public class TrainingFaqDaoImpl extends AbstractItemDao implements ITrainingFaqDao {
 
     private static final Logger LOG = LoggerFactory.getLogger(TrainingFaqDaoImpl.class);
     private final String FAQS_QUERY = "SELECT {" + FaqModel.PK + "} " + "FROM {" + FaqModel._TYPECODE + "}";
+    private final String FAQ_BY_ID_QUERY = "SELECT {" + FaqModel.PK + "} " +
+            "FROM {" + FaqModel._TYPECODE + "} " +
+            "WHERE {" + FaqModel.PK + "} = ?customerID";
 
     @Override
     public List<FaqModel> getFaqs() {
@@ -32,7 +34,18 @@ public class TrainingFaqDaoImpl extends AbstractItemDao implements ITrainingFaqD
     }
 
     @Override
-    public FaqModel putFaqs() {
-        return null;
+    public void putFaq(FaqModel faqModel) {
+
+        try
+        {
+            if (faqModel != null)
+                getModelService().save(faqModel);
+            else
+                throw new NullPointerException("FaqModel can't be null!");
+
+        } catch (Exception e)
+        {
+            LOG.error(e.getMessage(), e);
+        }
     }
 }
