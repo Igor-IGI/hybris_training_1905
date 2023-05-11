f4d35fe9b9d1637a6eb61172acb379adb6cfe11d
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-11 12:29:46 +0200

    add iterface FaqFacade

---
+++ bin/custom/trainingsocialsitefacade/src/de/hybris/platform/trainingsocialsitefacade/facade/ITrainingFaqFacade.java
@@ -0,0 +1,9 @@
+package de.hybris.platform.trainingsocialsitefacade.facade;
+
+import de.hybris.platform.trainingsocialsitefacade.data.FaqData;
+
+import java.util.List;
+
+public interface ITrainingFaqFacade {
+    public List<FaqData>getFaq();
+}
