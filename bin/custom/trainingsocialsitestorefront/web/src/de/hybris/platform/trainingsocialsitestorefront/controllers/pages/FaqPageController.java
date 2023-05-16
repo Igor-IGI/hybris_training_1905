d0ed37e72f5dbbe670a99d55ae7b3edc8c6164a8
Igor Popovic <62673509+Igor-IGI@users.noreply.github.com>
2023-05-16 14:35:54 +0200

    add faq cms page

--- bin/custom/trainingsocialsitestorefront/web/src/de/hybris/platform/trainingsocialsitestorefront/controllers/pages/FaqPageController.java
+++ bin/custom/trainingsocialsitestorefront/web/src/de/hybris/platform/trainingsocialsitestorefront/controllers/pages/FaqPageController.java
@@ -2,6 +2,8 @@ package de.hybris.platform.trainingsocialsitestorefront.controllers.pages;
 
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.AbstractController;
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractPageController;
+import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
+import de.hybris.platform.cms2.model.pages.ContentPageModel;
 import de.hybris.platform.trainingsocialsitefacade.data.FaqData;
 import de.hybris.platform.trainingsocialsitefacade.facade.impl.TrainingFaqFacadeImpl;
 import org.springframework.stereotype.Controller;
@@ -16,16 +18,20 @@ import java.util.List;
 @RequestMapping("/faq")
 public class FaqPageController extends AbstractPageController {
 
+    private static final String FAQ_CMS_PAGE = "faqCMSPage";
     @Resource(name = "trainingFaqFacade")
     private TrainingFaqFacadeImpl faqFacade;
 
     @RequestMapping(value = "/faqs", method = RequestMethod.GET)
-    public String getFaqs(final Model model) {
+    public String getFaqs(final Model model) throws CMSItemNotFoundException {
 
         List<FaqData> faqs = faqFacade.getFaq();
-        if (faqs != null)
-            model.addAttribute("faq", faqs);
+        model.addAttribute("faq", faqs);
 
+        // This will retrieve the created page by label which one we declared as "faqCMSPage"
+        final ContentPageModel faqCMSPage = getContentPageForLabelOrId(FAQ_CMS_PAGE);
+        storeCmsPageInModel(model, faqCMSPage);
+        setUpMetaDataForContentPage(model, faqCMSPage);
         return getViewForPage(model);
     }
 }
